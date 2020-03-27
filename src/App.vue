ab667722de8f0b354a2d785276b5b74f12c1e092
atogz <webdevatogz@gmail.co>
2020-03-27 21:08:58 +0300

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,32 @@
 <template>
   <div id="app">
-    <Header/>
-      <div class="hidden lg:block">
-          <Slider/>
-      </div>
-
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-
-import Header from './components/Header.vue'
-import Slider from "./components/Slider.vue";
-
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Slider
-  }
-}
-</script>
-
-
 <style>
 #app {
-    font-family: 'Montserrat', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-.fadeHeight-enter-active,
-.fadeHeight-leave-active {
-    transition: all 0.2s;
-    max-height: 360px;
-}
-.fadeHeight-enter,
-.fadeHeight-leave-to
-{
-    opacity: 0;
-    max-height: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-/*vue transitions*/
-.fade-enter,
-.fade-leave-active {
-    opacity: 0;
-}
-.fade-leave-active {
-    position: absolute;
+#nav {
+  padding: 30px;
 }
 
-.animated {
-    transition: all 0.5s;
-    /*display: flex;*/
-    width: 100%;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
-
-<style src="./assets/css/tailwind.css"></style>
-<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
