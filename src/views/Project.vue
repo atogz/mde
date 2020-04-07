b42c0379f3a74a2982683ba7e84dcd2e26552ad2
atogz <webdevatogz@gmail.co>
2020-04-07 11:14:55 +0300

    передача параметров в компонент проекта

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="w-full mt-6 pl-6 pr-6">
-        <h3>Проект №{{ projectId }}</h3>
+        <h3 class="text-3xl pb-6">Проект №{{ projectId }}</h3>
+        <img :src="project.imageLink" />
     </div>
 </template>
 
 <script>
   export default {
     name: "Project",
+    props: ['project'],
     data() {
       return {
         projectId: this.$route.params.id
       }
     },
     mounted() {
-
+        console.log('passed ', this.$route.params);
     }
   }
 </script>
